39fa22b2782954c808a5155eb327467c322001bf
Ivan Storchylo <i.storchylo@gmail.com>
2024-02-09 17:05:17 +0200

    Save binaries to root directory defalut output
    root_dir and outputdir should be specified at the root premake file

--- premake5.lua
+++ premake5.lua
@@ -4,8 +4,8 @@ project "ImGui"
 	cppdialect "C++17"
 	staticruntime "on"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir (root_dir .. "bin/" .. outputdir .. "/%{prj.name}")
+	objdir (root_dir .. "bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
